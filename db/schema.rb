6bfdcd6b170d115b6f8b405f637d1b5dab0b42cd
Nanashi1122 <os1122skyl@gmail.com>
2023-11-04 03:20:29 +0900

    ユーザー情報にプロフィール追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_28_161046) do
+ActiveRecord::Schema.define(version: 2023_11_03_180955) do
 
   create_table "reservations", force: :cascade do |t|
     t.date "checkin"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2023_10_28_161046) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
