00c831f88cae3f4aa6ee3ed0bc4a4d530e4ae16d
Nanashi1122 <os1122skyl@gmail.com>
2023-12-16 23:01:07 +0900

    bootstrapの導入方法の変更,予約時の予約確認モーダルを表示

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,12 @@
 //= require rails-ujs
 //= require turbolinks
 //= require jquery
-//= require bootstrap-sprockets
 //= require bootstrap
 //= require_tree .
+
+import 'bootstra;';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap';
+
+import $ from 'jquery';
+global.$ = jQuery;
