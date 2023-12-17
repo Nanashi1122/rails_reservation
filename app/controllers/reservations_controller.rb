d88afd79badd98b2e232ea6abb0d2f5963116fda
Nanashi1122 <os1122skyl@gmail.com>
2023-12-17 17:10:10 +0900

    予約確認時に範囲外の値を入力時にエラーメッセージを表示するように変更

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -15,12 +15,10 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new(params.require(:reservation).permit(:checkin, :checkout, :num_of_people, :room_id))
     @reservation.user_id = current_user.id
     if @reservation.save
-      flash[:notice] = "予定を確定しました"
       redirect_to action: :myreservation
     else
-      flash[:notice] = "予定の確定に失敗しました"
-      @rooms = Room.all
-      redirect_to controller: :rooms,action: :show
+      @rooms = Room.find(@reservation.room_id)
+      render 'rooms/show'
     end
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,8 +1,9 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :room
-
+  
   def total_nights
     (checkout - checkin).to_i
   end
+
 end
